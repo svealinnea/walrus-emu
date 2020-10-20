554c583980770afc20dad9c692cd75b328726a3a
Austin Schrader <austin.jschrader@gmail.com>
2020-10-20 10:07:11 -0700

    Add the scripts file

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+  
+});
+  $(".clickable").click(function(){
+    $("#emu-showing").toggle();
+    $("#emu-hidden").toggle();
+  })
+})
+
+can you see this
